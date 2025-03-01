2b0b8a950dd6750c156f394c8098883f450908f1
jade ko <dev.dkko@gmail.com>
2025-03-01 20:28:21 +0900

    refactor: 변수 추가

--- javascript/mission01/js/script.js
+++ javascript/mission01/js/script.js
@@ -1,10 +1,11 @@
 const buttons = document.querySelectorAll(".food-button");
 const menuItems = document.querySelectorAll(".menu-item");
 buttons.forEach(button => {
-    button.addEventListener("click", function() {
-        const category = this.dataset.category;
-        menuItems.forEach(item =>
-            item.style.display = ["all", item.dataset.category].includes(category) ? "flex" : "none"
-        );
+    button.addEventListener("click", function () {
+        const buttonCategory = this.dataset.category;
+        menuItems.forEach(item => {
+            const itemCategory = item.dataset.category;
+            item.style.display = ["all", itemCategory].includes(buttonCategory) ? "flex" : "none"
+        });
     });
 });
