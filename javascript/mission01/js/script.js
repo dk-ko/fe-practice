7e20fbd90dcab0d48250e2b140faf160470c7636
jade ko <dev.dkko@gmail.com>
2025-03-01 19:26:24 +0900

    feat: mission1 html/css

---
+++ javascript/mission01/js/script.js
@@ -0,0 +1,20 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     const imageContainer = document.getElementById("image-container");
+//     const images = [
+//         "food1.jpg",
+//         "food2.jpg",
+//         "food3.jpg",
+//         "bread.jpg",
+//         "breakfast.jpg",
+//         "cake.jpg",
+//         "potato.jpg",
+//         "shake.jpg"
+//     ];
+
+//     images.forEach(image => {
+//         const imgElement = document.createElement("img");
+//         imgElement.src = `images/${image}`;
+//         imgElement.classList = "w-full h-48 object-cover rounded-lg";
+//         imageContainer.appendChild(imgElement);
+//     })
+// })
