02a1c9bf9524054e426ddc412b7327f9e04d0fa3
jade ko <dev.dkko@gmail.com>
2025-03-01 20:07:00 +0900

    feat: mission1 html/css 수정

--- javascript/mission01/js/script.js
+++ javascript/mission01/js/script.js
@@ -1,20 +1,6 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     const imageContainer = document.getElementById("image-container");
-//     const images = [
-//         "food1.jpg",
-//         "food2.jpg",
-//         "food3.jpg",
-//         "bread.jpg",
-//         "breakfast.jpg",
-//         "cake.jpg",
-//         "potato.jpg",
-//         "shake.jpg"
-//     ];
-
-//     images.forEach(image => {
-//         const imgElement = document.createElement("img");
-//         imgElement.src = `images/${image}`;
-//         imgElement.classList = "w-full h-48 object-cover rounded-lg";
-//         imageContainer.appendChild(imgElement);
-//     })
-// })
+const buttons = document.querySelectorAll(".food-button");
+buttons.forEach(button => {
+    button.addEventListener("click", function() {
+        console.log(button);
+    })
+});
