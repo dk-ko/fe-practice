fcbe7afe05914b68c7f0c22d799a3d5baebd8ec3
jade ko <dev.dkko@gmail.com>
2025-03-01 20:25:26 +0900

    refactor: 함수형

--- javascript/mission01/js/script.js
+++ javascript/mission01/js/script.js
@@ -3,16 +3,8 @@ const menuItems = document.querySelectorAll(".menu-item");
 buttons.forEach(button => {
     button.addEventListener("click", function() {
         const category = this.dataset.category;
-        menuItems.forEach(item => {
-            if (category === "all") {
-                item.style.display = "flex";
-            } else {
-                if (item.dataset.category === category) {
-                    item.style.display = "flex";
-                } else {
-                    item.style.display = "none";
-                }
-            }
-        });
+        menuItems.forEach(item =>
+            item.style.display = ["all", item.dataset.category].includes(category) ? "flex" : "none"
+        );
     });
 });
