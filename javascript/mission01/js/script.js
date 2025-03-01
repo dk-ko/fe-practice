70f981a9d6a93a09fbaf8a6dca0c7bd0359a369d
jade ko <dev.dkko@gmail.com>
2025-03-01 20:22:07 +0900

    feat: 메뉴 필터링 기능 추가

--- javascript/mission01/js/script.js
+++ javascript/mission01/js/script.js
@@ -1,6 +1,18 @@
 const buttons = document.querySelectorAll(".food-button");
+const menuItems = document.querySelectorAll(".menu-item");
 buttons.forEach(button => {
     button.addEventListener("click", function() {
-        console.log(button);
-    })
+        const category = this.dataset.category;
+        menuItems.forEach(item => {
+            if (category === "all") {
+                item.style.display = "flex";
+            } else {
+                if (item.dataset.category === category) {
+                    item.style.display = "flex";
+                } else {
+                    item.style.display = "none";
+                }
+            }
+        });
+    });
 });
