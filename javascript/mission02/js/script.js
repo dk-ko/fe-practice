2bde211dfd2ca43891df106312a4d3181a29d912
jade ko <dev.dkko@gmail.com>
2025-03-02 18:26:29 +0900

    feat: mission2 가위바위보 게임 js 리팩토링

--- javascript/mission02/js/script.js
+++ javascript/mission02/js/script.js
@@ -2,6 +2,13 @@ let playerWinCount = 0;
 let computerWinCount = 0;
 let roundCount = 10;
 
+const resultDiv = document.querySelector('#result');
+const resultMessage = document.querySelector('#result-message');
+const playerWinCountDiv = document.querySelector('#player-win-count');
+const computerWinCountDiv = document.querySelector('#computer-win-count');
+const roundCountDisplay = document.querySelector('#round-count');
+const resetButton = document.querySelector('#reset');
+
 function computerPlay() {
     let choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random() * choices.length)];
@@ -16,66 +23,71 @@ function playRound(playerSelection, computerSelection) {
         (playerSelection === 'scissors' && computerSelection === 'paper')
     ) {
         playerWinCount++;
-        document.querySelector('#player-win-count').textContent = playerWinCount;
+        updateUI();
         return '플레이어 승리';
     } else {
         computerWinCount++;
-        document.querySelector('#computer-win-count').textContent = computerWinCount;
+        updateUI();
         return '컴퓨터 승리';
     }
 }
 
+function updateUI() {
+    playerWinCountDiv.textContent = playerWinCount;
+    computerWinCountDiv.textContent = computerWinCount;
+    roundCountDisplay.textContent = roundCount;
+}
+
 function game(playerSelection) {
+    if (roundCount === 0) {
+        alert('게임이 끝났습니다. 게임을 다시 시작하려면 [다시 시작] 버튼을 누르세요.');
+        return;
+    }
+    roundCount--;
+    updateUI();
+
     let computerSelection = computerPlay();
     let playResult = playRound(playerSelection, computerSelection);
     console.log(playResult);
-    document.querySelector('#result').style.display = 'block';
-    document.querySelector('#result-message').textContent = playResult;
+    resultDiv.style.display = 'block';
+    resultMessage.textContent = playResult;
 
-    if (roundCount === 0) {
-        endGame();
-    }
+    if (roundCount === 0) endGame();
 }
 
 function endGame() {
     if (playerWinCount > computerWinCount) {
         console.log('게임에서 이겼습니다.');
-        document.querySelector('#result').style.display = 'none';
-        document.querySelector('#result-message').textContent = '게임에서 이겼습니다.';
+        resultMessage.textContent = '게임에서 이겼습니다.';
     } 
     if (playerWinCount < computerWinCount) {
         console.log('게임에서 졌습니다.');
-        document.querySelector('#result').style.display = 'none';
-        document.querySelector('#result-message').textContent = '게임에서 졌습니다.';
+        resultMessage.textContent = '게임에서 졌습니다.';
     } 
     if (playerWinCount === computerWinCount) {
         console.log('무승부입니다.');
-        document.querySelector('#result').style.display = 'none';
-        document.querySelector('#result-message').textContent = '무승부입니다.';
+        resultMessage.textContent = '무승부입니다.';
     }
+    resultDiv.style.display = 'none';
+    resetButton.style.display = 'block';
+}
 
-    document.querySelector('#reset').style.display = 'block';
+function resetGame() {
+    playerWinCount = 0;
+    computerWinCount = 0;
+    roundCount = 10;
+
+    updateUI();
+    resultMessage.textContent = '';
+    resetButton.style.display = 'none';
 }
 
 document.querySelectorAll('.player-choice').forEach((button) => {
     button.addEventListener('click', function() {
-        if (roundCount === 0) {
-            alert('게임이 끝났습니다. 게임을 다시 시작하려면 [다시 시작] 버튼을 누르세요.');
-            return;
-        }
-        roundCount--;
-        document.querySelector('#round-count').textContent = roundCount;
         game(button.id);
     });
 });
 
-document.querySelector('#reset').addEventListener('click', function() {
-    playerWinCount = 0;
-    computerWinCount = 0;
-    roundCount = 10;
-    document.querySelector('#player-win-count').textContent = playerWinCount;
-    document.querySelector('#computer-win-count').textContent = computerWinCount;
-    document.querySelector('#round-count').textContent = roundCount;
-    document.querySelector('#result-message').textContent = '';
-    document.querySelector('#reset').style.display = 'none';
+resetButton.addEventListener('click', function() {
+    resetGame()
 });
