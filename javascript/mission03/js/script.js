1e2231f5151d60d1e4e45935c0851eee502d1af3
jade ko <dev.dkko@gmail.com>
2025-03-03 22:50:08 +0900

    feat: mission3 퀴즈앱

--- javascript/mission03/js/script.js
+++ javascript/mission03/js/script.js
@@ -2,6 +2,7 @@ const questionDiv = document.querySelector("#question");
 const answerDivs = document.querySelectorAll(".answer");
 const resetButton = document.querySelector(".reset-button");
 const nextButton = document.querySelector(".next-button");
+let gameCount = 3;
 
 document.addEventListener("DOMContentLoaded", () => {
   quiz();
@@ -25,6 +26,38 @@ function quiz() {
   const incorrectAnswers = getRandomNumber();
   const answers = [correctAnswer, incorrectAnswers];
   updateAnswerUI(answers);
+
+  if (gameCount === 0) {
+    nextButton.style.display = "none";
+    resetButton.style.display = "block";
+    gameCount = 3;
+    return;
+  }
+
+  answerDivs.forEach((answerDiv) => {
+    answerDiv.addEventListener("click", () => {
+      if (parseInt(answerDiv.textContent) === correctAnswer) {
+        document.body.style.backgroundColor = "green";
+      } else {
+        document.body.style.backgroundColor = "red";
+      }
+
+      answerDivs.forEach((div) => {
+        if (parseInt(div.textContent) === correctAnswer) {
+          div.style.backgroundColor = "green";
+        } else {
+          div.style.backgroundColor = "red";
+        }
+      });
+
+      nextButton.style.display = "block";
+      gameCount--;
+    });
+  });
+
+  nextButton.addEventListener("click", () => {
+    reset();
+  });
 }
 
 function updateQuestionUI(questionText) {
@@ -42,3 +75,17 @@ function updateAnswerUI(answers) {
 function shuffleArray(array) {
   return array.sort(() => Math.random() - 0.5);
 }
+
+function reset() {
+  document.body.style.backgroundColor = "gainsboro";
+  answerDivs.forEach((div) => {
+    div.style.backgroundColor = "gray";
+  });
+  nextButton.style.display = "none";
+  quiz();
+}
+
+resetButton.addEventListener("click", () => {
+  resetButton.style.display = "none";
+  reset();
+});
