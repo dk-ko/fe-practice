4b18eda14b54195d7d3c50f19a87a6d0d5f5ffff
jade ko <dev.dkko@gmail.com>
2025-03-04 00:10:40 +0900

    fix: mission3 퀴즈앱 비정상 동작

--- javascript/mission03/js/script.js
+++ javascript/mission03/js/script.js
@@ -3,6 +3,8 @@ const answerDivs = document.querySelectorAll(".answer");
 const resetButton = document.querySelector(".reset-button");
 const nextButton = document.querySelector(".next-button");
 let gameCount = 3;
+let correctAnswer = 0;
+let incorrectAnswers = 0;
 
 document.addEventListener("DOMContentLoaded", () => {
   quiz();
@@ -22,52 +24,64 @@ function generateQuestion() {
 }
 
 function quiz() {
-  const correctAnswer = generateQuestion();
-  const incorrectAnswers = getRandomNumber();
+  correctAnswer = generateQuestion();
+  incorrectAnswers = getRandomNumber();
   const answers = [correctAnswer, incorrectAnswers];
   updateAnswerUI(answers);
 
-  answerDivs.forEach((answerDiv) => {
-    answerDiv.addEventListener("click", () => {
-      if (parseInt(answerDiv.textContent) === correctAnswer) {
-        document.body.style.backgroundColor = "green";
-      } else {
-        document.body.style.backgroundColor = "red";
-      }
+  nextButton.style.display = "none";
 
-      answerDivs.forEach((div) => {
-        if (parseInt(div.textContent) === correctAnswer) {
-          div.style.backgroundColor = "green";
-        } else {
-          div.style.backgroundColor = "red";
-        }
-      });
-
-      nextButton.style.display = "block";
-      gameCount--;
-    });
-  });
-
-  nextButton.addEventListener("click", () => {
-    reset();
-  });
-
-  if (gameCount === 0) {
-    nextButton.style.display = "none";
+  if (gameCount > 0) {
+    console.log("게임 진행중");
+  } else {
+    console.log("게임이 끝났습니다.");
     resetButton.style.display = "block";
-    gameCount = 3;
     return;
   }
 }
+
+answerDivs.forEach((answerDiv) => {
+  answerDiv.addEventListener("click", () => {
+    if (parseInt(answerDiv.textContent) === correctAnswer) {
+      document.body.style.backgroundColor = "green";
+    } else {
+      document.body.style.backgroundColor = "red";
+    }
+
+    answerDivs.forEach((div) => {
+      if (parseInt(div.textContent) === correctAnswer) {
+        div.style.backgroundColor = "green";
+      } else {
+        div.style.backgroundColor = "red";
+      }
+    });
+
+    if (gameCount > 1) {
+      console.log(gameCount);
+      console.log("게임중");
+      nextButton.style.display = "block";
+    } else {
+      console.log(gameCount);
+      console.log("게임종료");
+      nextButton.style.display = "none";
+      resetButton.style.display = "block";
+    }
+  });
+});
+
+nextButton.addEventListener("click", () => {
+  gameCount--;
+  next();
+});
 
 function updateQuestionUI(questionText) {
   questionDiv.textContent = questionText;
 }
 
 function updateAnswerUI(answers) {
-  const shffledAnswers = shuffleArray([...answers]);
+  const shuffledAnswers = shuffleArray([...answers]);
   answerDivs.forEach((answerDiv, index) => {
-    answerDiv.textContent = shffledAnswers[index];
+    answerDiv.textContent = shuffledAnswers[index];
   });
   document.querySelector(".none").textContent = "정답이 없습니다.";
 }
@@ -76,12 +90,31 @@ function shuffleArray(array) {
   return array.sort(() => Math.random() - 0.5);
 }
 
+function next() {
+  document.body.style.backgroundColor = "gainsboro";
+  answerDivs.forEach((div) => {
+    div.style.backgroundColor = "gray";
+  });
+
+  nextButton.style.display = "none";
+
+  if (gameCount > 0) {
+    quiz();
+  } else {
+    resetButton.style.display = "block";
+  }
+}
+
 function reset() {
   document.body.style.backgroundColor = "gainsboro";
   answerDivs.forEach((div) => {
     div.style.backgroundColor = "gray";
   });
+
   nextButton.style.display = "none";
+  resetButton.style.display = "none";
+
+  gameCount = 3;
   quiz();
 }
 
